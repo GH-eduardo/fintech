3d33d5655d48dc92034b56b23f12e03f31119cbd
renza <jaorenzetti@outlook.com>
2024-06-23 20:31:08 -0300

    criação do docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose'
 import { UsersModule } from './users/users.module';
 import { TradesModule } from './trades/trades.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://0.0.0.0/fintech'),
     UsersModule,
     TradesModule,
